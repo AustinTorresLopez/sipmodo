020d40e51dadf6ee78779c7e374bdb4264eb0ac6
Austin Torres Lopez <torreslopez.austin@gmail.com>
2025-11-19 22:57:34 -0500

    deployed to vercel

--- sip-modo/src/supabaseClient.js
+++ sip-modo/src/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://exosygrmuqhntbswdkko.supabase.co";
-const supabaseAnonKey = "sb_publishable_39AdgPEMn-FwHzvgSkwx_g_rdkwCzEF";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+//const supabaseUrl = "https://exosygrmuqhntbswdkko.supabase.co";
+//const supabaseAnonKey = "sb_publishable_39AdgPEMn-FwHzvgSkwx_g_rdkwCzEF";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
